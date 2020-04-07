603149ca3879a1117a1684793aa12c8f4755402c
zajan <aniaaa94@gmail.com>
2020-04-07 22:09:00 +0200

    init

---
+++ src/main/java/com/ajna/bookaboat/BookABoatApplication.java
@@ -0,0 +1,13 @@
+package com.ajna.bookaboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookABoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookABoatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajna/bookaboat/BookABoatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajna.bookaboat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookABoatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
